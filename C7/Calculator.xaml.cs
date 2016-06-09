47e1f0e973f56f059ed2d2825b670466590fa7b6
Михаил Хлюстов < @DESKTOP-62CJP64>
2016-06-09 15:09:32 +0300

    Добавьте файлы проекта.

---
+++ C7/Calculator.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace C7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string Answer = null, Memory = null;
+        bool isReady = false;
+
+        private void Ans_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isReady)
+            {
+                txtBоx.VerticalAlignment = VerticalAlignment.Top;
+                txtBоx.Text = Answer;
+                isReady = true;
+                StаtusLаbel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                txtBоx.Text += Answer;
+            }
+
+        }
+
+        private void MR_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isReady)
+            {
+                txtBоx.VerticalAlignment = VerticalAlignment.Top;
+                txtBоx.Text = Answer;
+                isReady = true;
+                StаtusLаbel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                txtBоx.Text += Answer;
+            }
+
+        }
+
+        private void M_Click(object sender, RoutedEventArgs e)
+        {
+            if (isReady)
+            {
+                Memory = txtBоx.Text;
+            }
+            else
+            {
+                Memory = Answer;
+            }
+            MR.ToolTip = Memory;
+
+        }
+
+        private void MC_Click(object sender, RoutedEventArgs e)
+        {
+            Memory = null;
+            MR.ToolTip = "No value specified for memory.";
+
+        }
+
+        private void Sqrt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double b = double.Parse(txtBоx.Text);
+                b = Math.Sqrt(b);
+                txtBоx.Text = b.ToString();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "Error");
+
+            }
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!isReady)
+                {
+                    txtBоx.VerticalAlignment = VerticalAlignment.Top;
+                    StаtusLаbel.Visibility = Visibility.Hidden;
+                    txtBоx.Text = Answer;
+                    isReady = true;
+                }
+                else
+                {
+                    txtBоx.Text = txtBоx.Text.Substring(0, txtBоx.Text.Length - 1);
+                }
+            }
+            catch (Exception)
+            {
+                isReady = false;
+            }
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtBоx.VerticalAlignment = VerticalAlignment.Top;
+            StаtusLаbel.Visibility = Visibility.Hidden;
+            isReady = false;
+            Memory = null;
+            txtBоx.Text = null;
+            MR.ToolTip = "No value specified for memory.";
+            this.Height = 350;
+            this.Width = 525;
+            Answer = null;
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+            EvalLib evaluate = new EvalLib();
+            try
+            {
+                for (int i = 0; i < txtBоx.Text.Length; i++)
+                {
+                    if (txtBоx.Text[i] == '!' || ((txtBоx.Text[i] >= 40 && txtBоx.Text[i] <= 57) && txtBоx.Text[i] != 44) || txtBоx.Text[i] == 37)//ASCII
+                    {
+                        continue;
+                    }
+                    throw new KeyNotFoundException();
+                }
+                double result = evaluate.TextToValue(txtBоx.Text);
+                StаtusLаbel.Content = txtBоx.Text;
+                txtBоx.VerticalAlignment = VerticalAlignment.Bottom;
+                txtBоx.Text = "=" + result.ToString();
+                StаtusLаbel.Visibility = Visibility.Visible;
+                isReady = false;
+                Answer = result.ToString();
+
+            }
+            catch (KeyNotFoundException)
+            {
+                MessageBox.Show("Your input contains a character\nthat can't be calculated.", "Error");
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "Error");
+            }
+
+        }
+
+        private void dgtClick(object sender, RoutedEventArgs e)
+        {
+
+            if (!isReady)
+            {
+                txtBоx.VerticalAlignment = VerticalAlignment.Top;
+                txtBоx.Text = ((Button)sender).Content.ToString();
+                isReady = true;
+                StаtusLаbel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                txtBоx.Text += ((Button)sender).Content;
+            }
+
+
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            string about = "Write down your own Name, company, and what you want to tell the user about you.";
+            MessageBox.Show(about, "About My Calculator", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+        }
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            string help =
+             "        Just type any kind of complex or simple expression,\n" +
+             "the calculator have flexibility to produce accurate result.\n\n" +
+
+             "        You can use memory for store either a result or a \n" +
+             "complete expression. If there are any expression prevails \n" +
+             "in the screan, this will be stored if you press 'M+'. By \n" +
+             "pressing 'MR' you can add the memory content at any\n" +
+             "time to screan.'MC' will clear the memory. 'Ans' is an\n" +
+             "automatic memory. Every time you have a value for an\n" +
+             "expression, the resulted value will be stored in 'Ans'. You\n" +
+             "can only use it same as 'MR' but you can't assign anything\n" +
+             "to it as it is an automatic memory.\n\n" +
+
+
+             "         Reset will restore the calculator to its initial value.\n" +
+             "Remember, ALL MEMORY WILL BE LOST IF YOU CLICK 'Reset'\n" +
+             "You can use Del to clear the memory character by character.\n\n" +
+
+             "         Have fun using this calculator.";
+            MessageBox.Show(help, "Help", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+
+        }
+    }
+}
---
+++ C7/EvalLib.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C7
+{   
+    class EvalLib
+    {
+        private string sb = null;
+        private bool canUnaryMinus = true;
+
+        public double TextToValue(string text)
+        {
+            int l = text.Length;
+            polish(text);
+            double p = Eval_polish();
+            return p;
+        }
+
+        private void polish(string text)
+        {
+            char[] stact = new char[30];///stack used for operator storing.
+            int top = 1, i = 0;
+            stact[0] = '(';  ///initially push ( to stack
+            text = text + ")";
+            while (top != 0)
+            {
+                string a = null; ///a variable for storing operands
+                if ((text[i] > 47 && text[i] < 58) || text[i] == '.')
+                {
+                    try
+                    {
+                        while ((text[i] > 47 && text[i] < 58) || text[i] == '.')
+                        {
+                            a += text[i].ToString();
+
+                            i++;
+                        }
+                        sb += (a.ToString());
+                        sb += ","; ///sentinell to seperate operands
+                        canUnaryMinus = false;
+                    }
+                    catch (Exception)
+                    {
+                        return;
+                    }
+                }
+                else if (text[i] == '+' || text[i] == '-' || text[i] == '*' || text[i] == '/' || text[i] == '%' || text[i] == '!')
+                {
+                    int p = Precedance(stact[top - 1]), q = Precedance(text[i]);
+                    while (p >= q)
+                    {
+                        top--;
+                        sb += (stact[top].ToString());
+                        sb += ",";
+
+                        p = Precedance(stact[top - 1]);
+                    }
+                    if (q == 5)
+                        stact[top] = 'u';/// u represents unary minus.
+                    else
+                        stact[top] = text[i];
+                    i++;
+                    top++;
+                    canUnaryMinus = true;
+                }
+                else if (text[i] == '(')
+                {
+                    stact[top] = text[i];
+                    top++;
+                    i++;
+                }
+                else if (text[i] == ')')
+                {
+                    while (stact[top - 1] != '(')
+                    {
+                        top--;
+                        sb += (stact[top].ToString());
+                        sb += ",";
+                    }
+                    top--;
+                    i++;
+                }
+                else return;
+            }
+
+        }
+
+        private int Precedance(char a)///returns operator precedance
+        {
+            if (canUnaryMinus && a == '-')
+            {
+                canUnaryMinus = false;
+                return 5;
+            }
+            int precedenceNumber = 0;
+            switch (a)
+            {
+                case '+':
+                case '-':
+                    precedenceNumber = 1;
+                    break;
+                case '*':
+                case '/':
+                    precedenceNumber = 2;
+                    break;
+                case '%':
+                    precedenceNumber = 3;
+                    break;
+                case '!':
+                    precedenceNumber = 4;
+                    break;
+                case 'u':
+                    precedenceNumber = 5;
+                    break;
+            }
+            return precedenceNumber;
+        }
+
+        private double Eval_polish()
+        {
+            double[] stact = new double[32];
+            int top = 0, length = sb.Length - 1, i = 0;
+            try
+            {
+                while (i < length)
+                {
+                    string s = null;
+                    again:
+                    if (sb[i] == ',')
+                    {
+                        i++;
+                    }
+                    else if (sb[i] > 47 && sb[i] < 58 || sb[i] == '.')
+                    {
+                        s += sb[i].ToString();
+                        i++;
+                        goto again;
+                    }
+                    if (s != null)
+                    {
+                        stact[top] = double.Parse(s);
+                        top++;
+                        s = null;
+                    }
+                    if (sb[i] == '+' || sb[i] == '-' || sb[i] == '*' || sb[i] == '/' || sb[i] == '%')
+                    {
+
+                        double a, b;
+                        top--;
+                        a = stact[top];
+                        top--;
+                        b = stact[top];
+
+                        switch (sb[i])
+                        {
+                            case '+':
+                                a += b;
+                                break;
+                            case '-':
+                                a = b - a;
+                                break;
+                            case '*':
+                                a = b * a;
+                                break;
+                            case '/':
+                                a = b / a;
+                                break;
+                            case '%':
+                                a = b % a;
+                                break;
+                        }
+                        stact[top] = a;
+                        top++;
+                        i++;
+                    }
+                    else if (sb[i] == 'u' || sb[i] == '!')
+                    {
+                        double a, b = 1;
+                        top--;
+                        a = stact[top];
+                        if (sb[i] == 'u')
+                        {
+                            a = -a;
+                        }
+                        else
+                        {
+                            if (a > 170)
+                            {
+
+                                System.Windows.Forms.MessageBox.Show("Too Big for factorialization.\nIt can't be more then 170.", "Error!");
+                                return 0;
+                            }
+                            for (double mul = 1; mul <= a; mul++)
+                            {
+                                b *= mul;
+                            }
+                            a = b;
+                        }
+                        stact[top] = a;
+                        top++;
+                        i++;
+                    }
+
+                }
+
+                return stact[top - 1];
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message, "Error");
+                return 0;
+            }
+        }
+
+    }
+}
